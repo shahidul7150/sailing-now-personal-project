c91fe02a8f511d50949c17e76235f01fd1fa3061
Shahidul Islam <shahidulislam7150@gmail.com>
2022-05-30 14:08:19 +0600

    create register page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,70 +1,67 @@
 import React from 'react';
 import landing from '../../images/landing.jpg';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 const Login = () => {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
-  return ( 
-    <div className= 'sm:grid grid-cols-1 grid lg:grid-cols-2'>
-      <div className='w-2/4 mx-auto mt-28 py-12'>
+  return (
+    <div className="sm:grid grid-cols-1 grid lg:grid-cols-2">
+      <div className="w-2/4 mx-auto mt-28 py-12">
         <form onSubmit={handleSubmit(onSubmit)}>
-          
-
-        <div className="form-control w-full py-12">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="Your Email"
-                className="input input-bordered w-full "
-                {...register('email', {
-                  required: {
-                    value: true,
-                    message: 'Email is Required',
-                  },
-                  pattern: {
-                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                    message: 'Provide a valid Email',
-                  },
-                })}
-              />
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="Your Email"
-                className="input input-bordered w-full "
-                {...register('email', {
-                  required: {
-                    value: true,
-                    message: 'Email is Required',
-                  },
-                  pattern: {
-                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                    message: 'Provide a valid Email',
-                  },
-                })}
-              />
-             <input
-              className="btn btn-primary w-full mt-3 "
+          <div className="form-control w-full pb-12">
+          <h2 className='text-xl pt-1'>Please Login </h2>
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Your Email"
+              className="input input-bordered w-full "
+              {...register('email', {
+                required: {
+                  value: true,
+                  message: 'Email is Required',
+                },
+                pattern: {
+                  value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                  message: 'Provide a valid Email',
+                },
+              })}
+            />
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="input input-bordered w-full "
+              {...register('password', {
+                required: {
+                  value: true,
+                  message: 'Password is Required',
+                },
+                minLength: {
+                  value: 6,
+                  message: 'Must bre 6 characters or longer',
+                },
+              })}
+            />
+            <input
+                 className="btn border-0 text-[#293E60] bg-[#FADD75] w-full mt-3 "
               type="submit"
               value="Login"
             />
-              
-            </div>
-
-
-
-
-      {/* <input {...register("firstName", { required: true, maxLength: 20 })} />
-      <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-      <input type="number" {...register("age", { min: 18, max: 99 })} />
-      <input type="submit" /> */}
-    </form>
+            <small>
+              New to Tools User ?{' '}
+              <Link to="/register" className="text-primary">
+                Create New Account
+              </Link>
+            </small>
+          </div>
+        </form>
       </div>
-      <div className='text-end'>
+      <div className="text-end">
         <img src={landing} alt="" />
       </div>
     </div>
--- src/components/Login/Register/Register.js
+++ src/components/Login/Register/Register.js
@@ -1,9 +1,86 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import landing from '../../../images/landing.jpg'
 
 const Register = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
-    <div>
-      Registration
+    <div className="sm:grid grid-cols-1 grid lg:grid-cols-2">
+      <div className="w-2/4 mx-auto mt-20 py-12">
+        
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control w-full pb-12">
+          <h2 className='text-xl pt-1'>Please Registration </h2>
+            <label className="label">
+              <span className="label-text">Your name</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Your Name"
+              className="input input-bordered w-full "
+              {...register('name', {
+                required: {
+                  value: true,
+                  message: 'Name is Required',
+                },
+              
+              })}
+            />
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Your Email"
+              className="input input-bordered w-full "
+              {...register('email', {
+                required: {
+                  value: true,
+                  message: 'Email is Required',
+                },
+                pattern: {
+                  value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                  message: 'Provide a valid Email',
+                },
+              })}
+            />
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="input input-bordered w-full "
+              {...register('password', {
+                required: {
+                  value: true,
+                  message: 'Password is Required',
+                },
+                minLength: {
+                  value: 6,
+                  message: 'Must bre 6 characters or longer',
+                },
+              })}
+            />
+            <input
+              className="btn border-0 text-[#293E60] bg-[#FADD75] w-full mt-3 "
+              type="submit"
+              value="Join Now"
+            />
+            <small>
+              Already have an account ?{' '}
+              <Link to="/login" className="text-primary">
+               please login
+              </Link>
+            </small>
+          </div>
+        </form>
+      </div>
+      <div className="text-end">
+        <img src={landing} alt="" />
+      </div>
     </div>
   );
 };
