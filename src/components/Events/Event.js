3ec844aad803a0288e30a3fea805a16000b09388
Shahidul Islam <shahidulislam7150@gmail.com>
2022-06-04 20:06:20 +0600

    load events data from server

--- src/components/Events/Event.js
+++ src/components/Events/Event.js
@@ -1,16 +1,33 @@
 import React from 'react';
-
-const Event = ({ photo }) => {
+import './Events.css'
+const Event = ({ event }) => {
+  console.log(event);
   return (
-    <div class="card bg-base-100 shadow-xl">
+
+// ---------------------
+    
+    
+<div class="box">
+      <img src={ event.img}alt=""/>
+<div class="content">
+        <h3>{ event.title}</h3>
+        <p className='mb-2 text-xl font-light'>{ event.subTitle}</p>
+   <a href="#" class="btn">Booking Now</a>
+</div>
+</div>
+    
+    // -----------------------
+
+
+    // <div class="card bg-base-100 shadow-xl">
      
-      <figure>
-        <img width="100%"
-          src={photo.img}
-          alt="Shoes"
-        />
-      </figure>
-    </div>
+    //   <figure>
+    //     <img width="100%"
+    //       src={photo.img}
+    //       alt="Shoes"
+    //     />
+    //   </figure>
+    // </div>
   );
 };
 
--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -2,56 +2,69 @@ import React, { useEffect, useState } from 'react';
 import Carousel from '../Carusel/Carousel';
 import Footer from '../Footer/Footer';
 import Event from './Event';
-
+import './Events.css'
 const Events = () => {
   const [events, setEvents] = useState([]);
   useEffect(() => {
-    fetch('events.json')
+    fetch('http://localhost:5000/event')
       .then((res) => res.json())
       .then((data) => setEvents(data));
   }, []);
   return (
-      <div >
-          <div>
-              <Carousel/>
+    <div>
+      <div>
+        <Carousel />
+      </div>
+      <p className="text-center py-5 mt-3 text-xl font-bold ">Our Events </p>
+      {/* <div className=" grid sm:grid-cols-1 lg:grid-cols-2 gap-3 my-5 mx-12">
+        <div className="relative">
+          <img
+            src="https://www.chi.ac.uk/app/uploads/2021/09/Sailing-1024x576.jpg"
+            alt=""
+          />
+          <div className="absolute bottom-0 right-0">
+            <h2 className="text-3xl text-white px-4">Coming up next</h2>
+            <p className=" text-base bg-[#01206a60] text-white px-2 py-2 text-center mt-1">
+              {' '}
+              <button className="tbn-ghost">BOOK NOW</button>
+            </p>
           </div>
-          <p className='text-center py-5 mt-3 text-xl font-bold '>Our Events </p>
-          <div className=' grid sm:grid-cols-1 lg:grid-cols-2 gap-3 my-5 mx-12'>
-              
-              <div className='relative'>
-                  <img src="https://www.chi.ac.uk/app/uploads/2021/09/Sailing-1024x576.jpg" alt="" />
-                  <div className='absolute bottom-0 right-0'>
-                      <h2 className='text-3xl text-white px-4'>Coming up next</h2>
-                      <p className=' text-base bg-[#01206a60] text-white px-2 py-2 text-center mt-1'>BOOK NOW</p>
-                  </div>
-             </div>
-              <div className='relative'>
-                  <img src="https://resources.stuff.co.nz/content/dam/images/4/y/t/z/i/o/image.related.StuffLandscapeSixteenByNine.1420x800.22z12e.png/1627639428248.jpg" alt="" />
-                  <div className='absolute bottom-0 right-0'>
-                      <h2 className='text-3xl text-white px-4'>This Sunday</h2>
-                      <p className=' text-base bg-[#01206a60] text-white px-2 py-2 text-center mt-1'>BOOK NOW</p>
-                  </div>
-             </div>
-              
+        </div>
+        <div className="relative">
+          <img
+            src="https://resources.stuff.co.nz/content/dam/images/4/y/t/z/i/o/image.related.StuffLandscapeSixteenByNine.1420x800.22z12e.png/1627639428248.jpg"
+            alt=""
+          />
+          <div className="absolute bottom-0 right-0">
+            <h2 className="text-3xl text-white px-4">This Sunday</h2>
+            <p className=" text-base bg-[#01206a60] text-white px-2 py-2 text-center mt-1">
+              <button className="tbn-ghost">BOOK NOW</button>
+            </p>
+          </div>
+        </div>
+      </div> */}
+
+      <div className="grid lg:grid-cols-4 gap-5 mx-12 box-container">
+        {events.map((event) => (
+          <Event key={event._id} event={event}></Event>
+        ))}
       </div>
 
-          <div className='grid lg:grid-cols-4 gap-5 mx-12'>
-          {events.map((photo) => (
-        <Event key={photo._id} photo={photo}></Event>
-      ))}
-          </div>
-          
-          <div className='my-36'>
-              <h2 className='text-2xl text-center font-semibold'>Your Email For daily Event Update</h2>
+      <div className="my-36">
+        <h2 className="sm:text-xl text-2xl text-center font-semibold">
+          Your Email For daily Event Update
+        </h2>
 
-              
-              <div className=' text-center input-group justify-center mt-4'>
-                  <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs"/>
-                  <button className='btn btn-primary'>SUBMIT</button>
-           
-             </div>
-          </div>
-          <Footer/>
+        <div className="text-center input-group justify-center mt-4 w-full ">
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered  "
+          />
+          <button className="btn btn-primary">SUBMIT</button>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
