dae1a8b2d1f10072eb4b3826a3aa0202e9f6779a
Shahidul Islam <shahidulislam7150@gmail.com>
2022-06-04 21:05:47 +0600

    create create event page

--- src/components/Dashboard/AddEvent.js
+++ src/components/Dashboard/AddEvent.js
@@ -1,9 +1,82 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import './Dashboard.css'
 
 const AddEvent = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
     return (
-        <div>
-            Add event
+        <div className='w-3/5 mx-auto'>
+       
+            {/* <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("firstName", { required: true, maxLength: 20 })} />
+      <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+      <input type="number" {...register("age", { min: 18, max: 99 })} />
+      <input type="submit" />
+    </form> */}
+            
+
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control w-full pb-12">
+            <h2 className="text-xl pt-1">Create an Event</h2>
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Event Title"
+              className="input input-bordered w-full "
+              {...register('title', {
+                required: {
+                  value: true,
+                  message: 'Must include event title',
+                }
+              })}
+                    />
+                    
+         
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Sub-Title"
+              className="input input-bordered w-full "
+              {...register('subTitle', {
+                required: {
+                  value: true,
+                 
+                }
+              })}
+            />
+            <label className="label">
+              <span className="label-text">Event Image</span>
+            </label>
+            
+                    <div className='border border-zinc-300 py-2 rounded-lg w-5/5 '>
+                       
+                        <input className='file-upload' {...register("image")} type="file" />
+                    </div>
+
+            <input
+              className="btn border-0 text-[#293E60] bg-[#FADD75] w-full mt-3 "
+              type="submit"
+              value="Login"
+            />
+           
+          </div>
+        </form>
+
+
+
+
+
+
+
+
+{/* event end */}
+
         </div>
     );
 };
